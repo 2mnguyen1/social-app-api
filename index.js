9af3084c8b06620cbe4e8ea58116ed493f31a694
2mnguyen1 <2mnguyen1@gmail.com>
2022-05-07 15:41:42 -0700

    adding auth register and login

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, () => {
+    console.log("Conntected to MongoDB")
+})
+
+// middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use("/api/user", userRoute)
+app.use("/api/auth", authRoute)
+
+app.listen(3000, () => {
+    console.log("3000 PORT")
+})
+
