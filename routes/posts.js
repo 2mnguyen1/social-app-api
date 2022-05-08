da5dc7a1dbedfba01073a9b40725315ead69f09f
2mnguyen1 <2mnguyen1@gmail.com>
2022-05-08 16:23:26 -0700

    finishing post CRUD

--- routes/posts.js
+++ routes/posts.js
@@ -11,9 +11,41 @@ router.post('/posting', async (req, res, next) => {
         res.status(500).json(e)
     }
 })
-
 // update a post
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.userID === req.body.userID) {
+            await post.updateOne({
+                $set: req.body
+            })
+            res.status(200).json('Your post has been updated')
+        } else {
+            res.status(403).json('You are not the owner of this post')
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(500).json(e)
+    }
+})
+
 // delete a post
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.userID === req.body.userID) {
+            await Post.findByIdAndDelete(req.params.id)
+            res.status(200).json('Your post has been deleted')
+        } else {
+            res.status(403).json('You are not the owner of this post')
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(500).json(e)
+    }
+})
+
+
 // like a post
 // get a post
 // get timeline posts
