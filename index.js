39d940f005c50ec1a24b92bd936a059694d76737
2mnguyen1 <2mnguyen1@gmail.com>
2022-06-19 17:24:27 -0700

    adding mess and conver routes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversation");
+const messageRoute = require("./routes/message");
 const multer = require("multer");
 const path = require("path");
 
@@ -46,6 +48,9 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
--- routes/conversation.js
+++ routes/conversation.js
@@ -1,5 +1,19 @@
-const router = require('express').Router();
-const Conversation = require('../models/Conversation');
+const router = require("express").Router();
+const Conversation = require("../models/Conversation");
 
+// new conversation
+router.post("/", async (req, res) => {
+  const newConversation = new Conversation({
+    members: [req.body.senderID, req.body.receiverID],
+  });
+
+  try {
+      const savedConversation = await newConversation.save();
+  } catch (err) {
+      res.status(500).json(err)
+  }
+});
+
+// get conversation
 
 module.exports = router;
