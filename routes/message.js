ac679ab59d0136397d97313f65d4c6e5a16eeda7
2mnguyen1 <2mnguyen1@gmail.com>
2022-06-19 20:47:01 -0700

    adding get a mess middleware

--- routes/message.js
+++ routes/message.js
@@ -2,16 +2,26 @@ const router = require("express").Router();
 const Message = require("../models/Message");
 
 // add
-router.post('/', async (req, res) => {
-    const newMessage = new Message(req.body)
+router.post("/", async (req, res) => {
+  const newMessage = new Message(req.body);
 
-    try {
-        const savedMessage = await newMessage.save();
-        res.status(200).json(savedMessage);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
-
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// get a message
+router.get("/:conversationID", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationID: req.params.conversationID,
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
